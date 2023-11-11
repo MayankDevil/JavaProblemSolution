962fb8b4a251e6ec42f33f3e79594bae51ab52f5
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-11-11 16:22:47 +0000

    11-11-2023

---
+++ program24.java
@@ -0,0 +1,14 @@
+/*
+-   programmer:MayankDevil
+-   24. Write a Java program to check if a positive number is a palindrome or not
+*/ 
+class Test
+{
+    public static void main(String args[])
+    {
+        int n = 2;
+
+        System.out.println(" square root is "+(n * n));  // check value
+    }
+}
+// the end
