00a7692ebaea6f273b286830f33236ec041842d0
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-11-12 10:05:50 +0000

    12-11-2023

--- program24.java
+++ program24.java
@@ -6,9 +6,27 @@ class Test
 {
     public static void main(String args[])
     {
-        int n = 2;
+        int number = 1233210;    // get number
 
-        System.out.println(" square root is "+(n * n));  // check value
+        int original_number = number;   // save number as is it
+        
+        int reverse_number = 0;     // reverse number 
+
+        /* reverse number */
+        
+        while (number > 0)
+        {   
+            reverse_number *= 10 + (number % 10);   // add last digit
+            
+            number /= 10;  // remove last digit
+        }
+        
+        /* if number equal to self after reverse is called palindrome */
+        
+        if (original_number == reverse_number)
+            System.out.println(original_number + " is a palindrome number.");
+        else
+            System.out.println(original_number + " is not a palindrome number.");
     }
 }
 // the end
