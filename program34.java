da61eab71d22f553c204e95601e21515538ac3d7
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-11-13 13:19:17 +0000

    13-11-2023

--- program34.java
+++ program34.java
@@ -1,19 +1,14 @@
 /*
 -   programmer:MayankDevil
--   33. Java Program to find area of Prism
+-   34. Java Program to find volume of sphere
 */ 
 class Test
 {
     public static void main(String args[])
-    {
-        double side = 5.0;
+    {        
+        double radius = 2.0;
         
-        System.out.println("Area of the cube is: " + (6 * side * side));
-
-        /*
-        - A prism is a three-dimensional solid shape that has two identical bases and flat faces that connect the bases. The bases are parallel to each other and have the same shape.
-        - let take cube that area is [ 6 side square ]
-        */
+        System.out.printf("The volume of the sphere is %.2f cubic units.",((4.0 / 3.0) * Math.PI * Math.pow(radius, 3)));
     }
 }
 // the end
